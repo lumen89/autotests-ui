b1237ab58ba704538bae9401a1b6395751382d3d
lumen89 <kalinin499@gmail.com>
2025-05-21 16:25:09 +0300

    Интеграция Pytest и Playwright. Практика написания автотестов.

---
+++ tests/test_courses.py
@@ -0,0 +1,33 @@
+from playwright.sync_api import sync_playwright, expect
+
+
+def test_empty_courses_list():
+    with sync_playwright() as playwright:
+        browser = playwright.chromium.launch(headless=False)
+        context = browser.new_context()
+        page = context.new_page()
+
+        page.goto("https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/auth/registration")
+
+        email_input = page.get_by_test_id('registration-form-email-input').locator('input')
+        email_input.fill("user.name@gmail.com")
+
+        password_input = page.get_by_test_id('registration-form-username-input').locator('input')
+        password_input.fill("username")
+
+        password_input = page.get_by_test_id('registration-form-password-input').locator('input')
+        password_input.fill("password")
+
+        login_button = page.get_by_test_id('registration-page-registration-button')
+        login_button.click()
+
+        context.storage_state(path="browser-state.json")
+
+    with sync_playwright() as playwright:
+        browser = playwright.chromium.launch(headless=False)
+        context = browser.new_context(storage_state="browser-state.json")
+        page = context.new_page()
+
+        page.goto("https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/courses")
+        expect(page.get_by_test_id('courses-list-toolbar-title-text')).to_have_text("Courses")
+        expect(page.get_by_test_id('courses-list-empty-view-title-text')).to_have_text("There is no results")
